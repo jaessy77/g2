cfc0105f7038a4acd7e62f7333e56e17a38057ff
Marco Jassmann <jaessy77@gmail.com>
2020-08-03 12:53:26 +0200

    added

--- g2core/board/OvK_Shield.mk
+++ g2core/board/OvK_Shield.mk
@@ -3,46 +3,56 @@
 
 
 # To compile:
-#   make BOARD=g2v9k
+#   make BOARD=gShield
+# Or:
+#   make BOARD=shopbotShield
 
 # You can also choose a CONFIG from boards.mk:
-#   make CONFIG=ShapeokoDualY BOARD=g2v9k
+#   make CONFIG=ShapeokoDualY BOARD=gShield
 
+
+
+# Backward compatibility with old projects that use PLATFORM instead, but with a warning:
+ifneq ("$(PLATFORM:-)","")
+    $(warning Using PLATFORM value of $(PLATFORM) as BOARD.)
+    $(warning Please swich to using BOARD on the command line.)
+    BOARD = $(PLATFORM)
+endif
 
 
 ##########
-# BOARDs for use directly from the make command line (with default settings) or by the CONFIGS above.
+# BOARDs for use directly from the make command line (with default settings) or by CONFIGs.
 
-ifeq ("$(BOARD)","OvKa")
-    BASE_BOARD=ovk
-    DEVICE_DEFINES += MOTATE_BOARD="OvKa"
+ifeq ("$(BOARD)","ovk_shield")
+    # This is a due with a OvK shield. We'll use the Due platform, but set defines
+    # for the code to get the pinout right.
+
+    BASE_BOARD = g2core-due
+    DEVICE_DEFINES += MOTATE_BOARD="ovk_shield"
     DEVICE_DEFINES += SETTINGS_FILE=${SETTINGS_FILE}
 endif
 
-#ifeq ("$(BOARD)","g2v9k")
-#    BASE_BOARD=g2v9
-#    DEVICE_DEFINES += MOTATE_BOARD="G2v9k"
-#    DEVICE_DEFINES += SETTINGS_FILE=${SETTINGS_FILE}
-#endif
-
 
 ##########
-# The general ovk BASE_BOARD.
+# The general g2core-due BASE_BOARD.
 
-ifeq ("$(BASE_BOARD)","ovk")
-    _BOARD_FOUND = 1
-
-    FIRST_LINK_SOURCES += $(sort $(wildcard ${MOTATE_PATH}/Atmel_sam_common/*.cpp)) $(sort $(wildcard ${MOTATE_PATH}/Atmel_sam3x/*.cpp))
-
-    # Set CHIP and export it for GDB to see
-    CHIP = SAM3X8C
-    export CHIP
-    CHIP_LOWERCASE = sam3x8c
-
-    BOARD_PATH = ./board/OvK
-    SOURCE_DIRS += ${BOARD_PATH} device/step_dir_driver
-
-    PLATFORM_BASE = ${MOTATE_PATH}/platform/atmel_sam
-
-    include $(PLATFORM_BASE).mk
-endif
+#ifeq ("$(BASE_BOARD)","g2core-due")
+#    _BOARD_FOUND = 1
+#
+#    DEVICE_DEFINES += MOTATE_CONFIG_HAS_USBSERIAL=1
+#
+#    FIRST_LINK_SOURCES += $(sort $(wildcard ${MOTATE_PATH}/Atmel_sam_common/*.cpp)) $(sort $(wildcard ${MOTATE_PATH}/Atmel_sam3x/*.cpp))
+#
+#    CHIP = SAM3X8E
+#    export CHIP
+#    CHIP_LOWERCASE = sam3x8e
+#
+#    # Note: we call it "g2core-due" instead of "due" since the Motate built-in provides
+#    # a "due" BASE_BOARD.
+#    BOARD_PATH = ./board/OvK_Shield
+#    SOURCE_DIRS += ${BOARD_PATH} device/step_dir_driver
+#
+#    PLATFORM_BASE = ${MOTATE_PATH}/platform/atmel_sam
+#
+#    include $(PLATFORM_BASE).mk
+#endif
